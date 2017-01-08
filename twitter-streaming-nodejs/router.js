fbc5f028b788e360ed694487be53e900000b5ca0
cclzxl <cheng.cheng.chg@gmail.com>
2017-01-09 00:51:37 +0100

    rest api

--- twitter-streaming-nodejs/router.js
+++ twitter-streaming-nodejs/router.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 var http = require("http");
 
-router.get('/emoji_frequence', function(req, res, next) {
-    /* Verify if search is undefined */
-    /*optional params */
+router.get('/tweets', function(req, res, next) {
+    var query =  '/tweets/tweets/';
+    if(req.query.count)
+    {
+        query += '$cmd/?filter_count=tweets&limit=1';
+    }
     var options = 
     {
         host: '127.0.0.1',
         port: 28017,
-        path: '/tweets/tweets/',
+        path: query,
         method: 'GET'
     };
 
@@ -34,4 +37,99 @@ router.get('/emoji_frequence', function(req, res, next) {
 
 });
 
+router.get('/emoji_frequence', function(req, res, next) {
+    var query =  '/tweets/emoji_frequence/?find';
+    
+    var options = 
+    {
+        host: '127.0.0.1',
+        port: 28017,
+        path: query,
+        method: 'GET'
+    };
+
+        var getRequest = http.request(options, function(response)
+        {
+            var result = "";
+            response.on('data', function (chunk) {
+                result +=chunk;
+            });
+            response.on('end', function() {
+                res.write(result);
+                res.end();
+                  
+            });
+               
+        });
+        getRequest.on('error', (e) => {
+            console.log(`problem with request: ${e.message}`);
+            res.status(500).json({error:"Internal Server Error:	Please try later"});
+        });
+        getRequest.end();   
+
+});
+
+router.get('/emoji_day_frequence', function(req, res, next) {
+    var query =  '/tweets/emoji_day_frequence/?find';
+    
+    var options = 
+    {
+        host: '127.0.0.1',
+        port: 28017,
+        path: query,
+        method: 'GET'
+    };
+
+        var getRequest = http.request(options, function(response)
+        {
+            var result = "";
+            response.on('data', function (chunk) {
+                result +=chunk;
+            });
+            response.on('end', function() {
+                res.write(result);
+                res.end();
+                  
+            });
+               
+        });
+        getRequest.on('error', (e) => {
+            console.log(`problem with request: ${e.message}`);
+            res.status(500).json({error:"Internal Server Error:	Please try later"});
+        });
+        getRequest.end();   
+
+});
+
+router.get('/emoji_pair_frequence', function(req, res, next) {
+    var query =  '/tweets/emoji_pair_frequence/?find';
+    
+    var options = 
+    {
+        host: '127.0.0.1',
+        port: 28017,
+        path: query,
+        method: 'GET'
+    };
+
+        var getRequest = http.request(options, function(response)
+        {
+            var result = "";
+            response.on('data', function (chunk) {
+                result +=chunk;
+            });
+            response.on('end', function() {
+                res.write(result);
+                res.end();
+                  
+            });
+               
+        });
+        getRequest.on('error', (e) => {
+            console.log(`problem with request: ${e.message}`);
+            res.status(500).json({error:"Internal Server Error:	Please try later"});
+        });
+        getRequest.end();   
+
+});
 module.exports = router;
